dffd58a7fc212c4b86b00ff9fdf9a30a749ef74f
unknown <djaredchase@gmail.com>
2022-06-18 01:05:02 -0400

    finished the little practice for work a little while ago, finally pushing

--- src/MyProjects.js
+++ src/MyProjects.js
@@ -1,4 +1,5 @@
-// this component is for something work related, not relevant to this Todo App
+// This component is to demonstrate some basic React knowledge for work (React work upcoming and I'm on a node project)
+// Not at all related to this Todo App
 import Table from 'react-bootstrap/Table';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
@@ -9,6 +10,7 @@ import { useState } from 'react';
 const MyProjects = () => {
     const [show, setShow] = useState(false);
     const [modalContent, setModalContent] = useState('');
+    const [projects, setProjects] = useState(dummyProjects);
 
     const handleClose = () => {
         setShow(false);
@@ -18,6 +20,20 @@ const MyProjects = () => {
         setModalContent(projectName);
         setShow(true);
     }
+    // Below is commented code for how I would fetch data from the backend
+
+    // const getProjects = async () => {
+    //     try {
+    //         const projects = await axios.get('/projects');
+    //         setProjects(projects);
+    //     } catch(err) {
+    //         // axios automatically captures error for response not in 200 range
+    //         // handle error here
+    //     }
+    // }
+    // useEffect(() => {
+    //     getProjects();
+    // }, []);
 
     return (
         <div style={{ padding: '10px' }}>
@@ -40,7 +56,7 @@ const MyProjects = () => {
                 </thead>
                 <tbody>
                     {
-                        dummyProjects.map((proj, index) => (
+                        projects.map((proj, index) => (
                             <tr key={index}>
                                 <td><FormCheck /></td>
                                 <td>{proj.siteNumber}</td>
@@ -103,6 +119,18 @@ const dummyProjects = [
         projectNumber: 7891011,
         projectName: 'Dummy Project 2',
         projectType: 'Modification',
+        vendorTurnkey: '',
+        rtifDate: '',
+        wfStartDate: '6/09/2022',
+        wfStep: 'Validating Site Info - In progress',
+    },
+    {
+        siteNumber: 343434,
+        siteName: 'Dummy Site 3',
+        siteType: 'Other Type',
+        projectNumber: 6767676,
+        projectName: 'Dummy Project 3',
+        projectType: 'Initial Build',
         vendorTurnkey: '',
         rtifDate: '',
         wfStartDate: '6/09/2022',
